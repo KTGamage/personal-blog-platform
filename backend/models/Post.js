db834ada5ce9611d3ecbc6691e7966c0ca07042e
Kasun Tharaka <kasuntharaka18628@gmail.com>
2025-11-21 19:56:53 +0530

    built model shema for post

---
+++ backend/models/Post.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 200
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  coverImage: {
+    type: String,
+    default: ''
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  tags: [{
+    type: String,
+    trim: true
+  }],
+  category: {
+    type: String,
+    trim: true
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  likesCount: {
+    type: Number,
+    default: 0
+  },
+  commentsCount: {
+    type: Number,
+    default: 0
+  },
+  isPublished: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+// Update counts before saving
+postSchema.pre('save', function(next) {
+  this.likesCount = this.likes.length;
+  next();
+});
+
+// Index for search functionality
+postSchema.index({
+  title: 'text',
+  content: 'text',
+  tags: 'text'
+});
+
+module.exports = mongoose.model('Post', postSchema);
