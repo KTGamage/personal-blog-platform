018fcf5e695764561c8ae635553d4f6d27226086
Kasun Tharaka <kasuntharaka18628@gmail.com>
2025-11-24 22:49:27 +0530

    recover changes

--- backend/server.js
+++ backend/server.js
@@ -80,9 +80,7 @@ app.use(cors({
     'https://https://personal-blog-platform-blush.vercel.app'
 
   ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.static('uploads'));
